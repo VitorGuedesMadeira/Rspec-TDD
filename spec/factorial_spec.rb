c47112e9501b1bd4e35f9e372abec7137b93dd5a
wayungi <fwayungi@gmail.com>
2022-10-06 22:43:23 +0300

    Add test for negative values

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -3,7 +3,11 @@ require './solver'
 describe "Solver" do
     solver = Solver.new
 
-    it "Its should return 5! 120" do
+    it "Should return 5! 120" do
         expect(solver.factorial(5)).to eql(120)
+    end
+
+    it "Should raise an error when a number < 0 is passed" do
+        expect { solver.factorial(-5) }.to raise_error
     end
 end
