598841e1a6006fa4bb031abeae4271c0d20a891e
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-10-06 17:40:45 -0300

    Add new tests, fix linters and add readme file

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# Rspec-TDD
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# Rspec-TDD 🔢✅
+ 
+> This project was made to practice Test Driven Development with the help of RSpec. 
+
+## Built With
+
+- [x] Ruby (programming language)
+- [x] Linters: [Rubocop](https://rubocop.org/)
+- [x] RSpec (testing)
+- [x] Visual Studio Code (code editor)
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisites
+
+- [x] A web browser like Google Chrome.
+- [x] A code editor like Visual Studio Code with Git and Ruby.
+
+You can check if Git is installed by running the following command in the terminal.
+```
+$ git --version
+```
+
+Likewise for Ruby installation.
+```
+$ ruby --version && irb
+```
+
+### Setup
+
+Clone the repository using the GitHub link provided below.
+
+### Install
+
+In the terminal, go to your file directory and run this command.
+```
+$ git clone https://github.com/VitorGuedesMadeira/Rspec-TDD.git
+```
+
+Run the command to install the necessary GEMS
+```
+$ bundle install
+```
+
+### Usage
+
+Kindly modify the files as needed.
+
+## Author
+
+👤 **Vitor Guedes Madeira**
+- GitHub: [@vitorguedesmadeira](https://github.com/VitorGuedesMadeira)
+- Twitter: [@CDahguedes](https://twitter.com/CDahguedes)
+- LinkedIn: (https://www.linkedin.com/in/vitor-guedes-madeira/)
+
+👤 **Francis Wayungi**
+
+- GitHub: [@githubhandle](https://github.com/wayungi)
+- Twitter: [@twitterhandle](https://twitter.com/FrancisWayungi)
+- LinkedIn: [LinkedIn](https://linkedin.com/in/francis-wayungi-3aa626231)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](hhttps://github.com/VitorGuedesMadeira/Morse-Decoder/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- [Microverse](https://www.microverse.org/)
+- Coding Partners
+- Code Reviewers
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -8,8 +8,16 @@ context 'factorialise' do
       expect(solver.factorial(5)).to eql(120)
     end
 
+    it 'Should not return 5! 150' do
+      expect(solver.factorial(5)).not_to eql(150)
+    end
+
     it 'Should raise an error when a number < 0 is passed' do
       expect { solver.factorial(-5) }.to raise_error('Negative value Error')
+    end
+
+    it 'Should not raise an error when a number >= 0 is passed' do
+      expect { solver.factorial(5) }.not_to raise_error('Negative value Error')
     end
   end
 end
@@ -25,6 +33,14 @@ context 'Reverse string' do
     it 'Should not return hello when given hello' do
       expect(solver.reverse('hello')).not_to eql('hello')
     end
+
+    it 'Should return esrevorciM when given Microverse' do
+      expect(solver.reverse('Microverse')).to eql('esrevorciM')
+    end
+
+    it 'Should not return Microverse when given Microverse' do
+      expect(solver.reverse('Microverse')).not_to eql('Microverse')
+    end
   end
 end
 
@@ -36,16 +52,32 @@ context 'fizzbuzz' do
       expect(solver.fizzbuzz(6)).to eql('fizz')
     end
 
+    it 'Should not return buzz when number is divisible by 3' do
+      expect(solver.fizzbuzz(6)).not_to eql('buzz')
+    end
+
     it 'Should return buzz when number is divisible by 5' do
       expect(solver.fizzbuzz(20)).to eql('buzz')
+    end
+
+    it 'Should not return fizz when number is divisible by 5' do
+      expect(solver.fizzbuzz(20)).not_to eql('fizz')
     end
 
     it 'Should return fizzbuzz when number is divisible by 3 & 5' do
       expect(solver.fizzbuzz(30)).to eql('fizzbuzz')
     end
 
+    it 'Should not return buzzfizz when number is divisible by 3 & 5' do
+      expect(solver.fizzbuzz(30)).not_to eql('buzzfizz')
+    end
+
     it 'Should return number as a string when number is not divisible by 3 or 5' do
       expect(solver.fizzbuzz(8)).to eql('8')
+    end
+
+    it 'Should return number as a number when number is not divisible by 3 or 5' do
+      expect(solver.fizzbuzz(8)).not_to eql(8)
     end
   end
 end
